06f681d843d896b7718bd1afb4ef6bd1662e0366
Konstantin Karzanov <k.karzanov@gmail.com>
2022-12-16 11:49:20 +0100

    update(patch) only changed data in forms

--- src/stores/persistent.js
+++ src/stores/persistent.js
@@ -6,10 +6,14 @@ export function usePersistent(schema, url, id) {
   const item = ref(schema);
   const persist = ref(false);
   const router = useRouter();
+  let original = null;
 
   if (id) {
     const { data } = apiClient.read(url + id + "/");
-    const setItem = (newData) => (item.value = newData);
+    const setItem = (newData) => {
+      item.value = newData;
+      original = { ...newData };
+    };
     if (data.value) setItem(data.value);
     watch(data, setItem);
   }
@@ -20,13 +24,22 @@ export function usePersistent(schema, url, id) {
       apiClient.delete(url + id + "/").then(() => back());
     }
   }
+  function update() {
+    const current = { ...item.value };
+    const changed = Object.keys(current).reduce((changed, key) => {
+      if (current[key] !== original[key]) changed[key] = current[key];
+      return changed;
+    }, {});
+    if (Object.keys(changed).length) {
+      apiClient.update(url + id + "/", changed).then(() => back());
+    } else {
+      back();
+    }
+  }
   function save() {
     persist.value = true;
-    if (id) {
-      apiClient.replace(url + id + "/", item.value).then(() => back());
-    } else {
-      apiClient.create(url, item.value).then(() => back());
-    }
+    if (id) update();
+    else apiClient.create(url, item.value).then(() => back());
   }
   function back() {
     router.back();
