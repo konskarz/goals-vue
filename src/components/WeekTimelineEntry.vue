549ddeb031684edff9e126b137521fcf82a9d059
Konstantin Karzanov <k.karzanov@gmail.com>
2022-12-24 01:17:12 +0100

    restore undraggable, test draggable backup

--- src/components/WeekTimelineEntry.vue
+++ src/components/WeekTimelineEntry.vue
@@ -1,7 +1,4 @@
 <script setup>
-import "drag-drop-touch";
-import { date } from "quasar";
-import apiClient from "stores/api.client";
 import TaskListItem from "./TaskListItem.vue";
 
 const props = defineProps({
@@ -17,47 +14,16 @@ const subtitle = [
   // props.week.month,
   props.week.day,
 ].join(" · ");
-
-function onDragStart(e, task) {
-  const data = {
-    id: task.id,
-    day: date.getDayOfWeek(task.planned),
-    week: props.week.week,
-  };
-  e.dataTransfer.setData("text", JSON.stringify(data));
-  e.dataTransfer.dropEffect = "move";
-}
-function onDrop(e, week) {
-  e.preventDefault();
-  const data = JSON.parse(e.dataTransfer.getData("text"));
-  if (data.week === week.week) return;
-  const newDate = date.addToDate(new Date(week.day), {
-    days: data.day - 1,
-  });
-  apiClient
-    .update("/api/v2/tasks/" + data.id + "/", {
-      planned: newDate.toISOString(),
-    })
-    .then(() => emit("mutate"));
-}
 </script>
 
 <template>
-  <q-timeline-entry
-    :subtitle="subtitle"
-    @dragover.prevent
-    @dragenter.prevent
-    @drop="onDrop($event, week)"
-  >
-    <q-list>
+  <q-timeline-entry :subtitle="subtitle">
+    <q-list v-if="week.tasks && week.tasks.length">
       <TaskListItem
         v-for="task in week.tasks"
         :key="task.id"
         :task="task"
-        draggable="true"
         @mutate="$emit('mutate')"
-        @dragstart="onDragStart($event, task)"
-        @touchmove:native="(e) => {}"
       />
     </q-list>
   </q-timeline-entry>
---
+++ src/components/WeekTimelineEntryDrop.vue
@@ -0,0 +1,64 @@
+<script setup>
+import "drag-drop-touch";
+import { date } from "quasar";
+import apiClient from "stores/api.client";
+import TaskListItem from "./TaskListItem.vue";
+
+const props = defineProps({
+  week: {
+    type: Object,
+    required: true,
+  },
+});
+const emit = defineEmits(["mutate"]);
+const subtitle = [
+  "Week " + props.week.week,
+  props.week.quarter,
+  // props.week.month,
+  props.week.day,
+].join(" · ");
+
+function onDragStart(e, task) {
+  const data = {
+    id: task.id,
+    day: date.getDayOfWeek(task.planned),
+    week: props.week.week,
+  };
+  e.dataTransfer.setData("text", JSON.stringify(data));
+  e.dataTransfer.dropEffect = "move";
+}
+function onDrop(e, week) {
+  e.preventDefault();
+  const data = JSON.parse(e.dataTransfer.getData("text"));
+  if (data.week === week.week) return;
+  const newDate = date.addToDate(new Date(week.day), {
+    days: data.day - 1,
+  });
+  apiClient
+    .update("/api/v2/tasks/" + data.id + "/", {
+      planned: newDate.toISOString(),
+    })
+    .then(() => emit("mutate"));
+}
+</script>
+
+<template>
+  <q-timeline-entry
+    :subtitle="subtitle"
+    @dragover.prevent
+    @dragenter.prevent
+    @drop="onDrop($event, week)"
+  >
+    <q-list v-if="week.tasks && week.tasks.length">
+      <TaskListItem
+        v-for="task in week.tasks"
+        :key="task.id"
+        :task="task"
+        draggable="true"
+        @dragstart="onDragStart($event, task)"
+        @touchmove:native="(e) => {}"
+        @mutate="$emit('mutate')"
+      />
+    </q-list>
+  </q-timeline-entry>
+</template>
