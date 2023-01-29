2fb445e837301396c9b77195068369ff726be5fc
Konstantin Karzanov <k.karzanov@gmail.com>
2023-01-29 21:35:44 +0100

    more confetti

--- src/components/TaskListItem.vue
+++ src/components/TaskListItem.vue
@@ -20,13 +20,40 @@ const progress = computed(() =>
 const caption = computed(() =>
   [props.task.performance, "of", props.task.target].join(" ")
 );
+function fireworks() {
+  var duration = 15 * 1000;
+  var animationEnd = Date.now() + duration;
+  var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+  function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+  var interval = setInterval(function () {
+    var timeLeft = animationEnd - Date.now();
+    if (timeLeft <= 0) {
+      return clearInterval(interval);
+    }
+    var particleCount = 50 * (timeLeft / duration);
+    confetti(
+      Object.assign({}, defaults, {
+        particleCount,
+        origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+      })
+    );
+    confetti(
+      Object.assign({}, defaults, {
+        particleCount,
+        origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+      })
+    );
+  }, 250);
+}
 function showProgressDialog() {
   $q.dialog({
     component: ProgressDialog,
     componentProps: { task: props.task },
   }).onOk((data) => {
     if (data.performance !== props.task.performance) {
-      if (data.performance >= props.task.target) confetti();
+      if (data.performance >= props.task.target) fireworks();
       apiClient
         .update("/tasks/" + props.task.id + "/", {
           performance: data.performance,
