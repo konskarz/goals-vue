e0a6cf055989ccf38b51d8d19e0fa1de57a1888e
Konstantin Karzanov <k.karzanov@gmail.com>
2023-01-30 16:42:12 +0100

    fix calendar filter

--- src/views/CalendarView.vue
+++ src/views/CalendarView.vue
@@ -5,16 +5,16 @@ import apiClient from "../stores/api.client";
 import GoalSelect from "../components/GoalSelect.vue";
 import WeekTimelineEntry from "../components/WeekTimelineEntry.vue";
 
-const GOAL_FILTER_KEY = "Goal-Filter";
+const TASKS_FILTER_KEY = "Tasks-Filter";
 const currentDate = date.startOfDate(new Date(), "day");
 const currentMonday = date.subtractFromDate(currentDate, {
   days: date.getDayOfWeek(currentDate) - 1,
 });
 const currentWeek = date.formatDate(currentDate, "YYYY-w");
-const goal = ref(null);
 const filter = ref({
   done: true,
   recurring: true,
+  goal: null,
 });
 const { data, mutate } = apiClient.read("/tasks/");
 const tasks = computed(() =>
@@ -24,7 +24,7 @@ const tasks = computed(() =>
       return false;
     } else if (passed && filter.value.recurring && task.group_id) {
       return false;
-    } else if (goal.value && goal.value !== task.goal) {
+    } else if (filter.value.goal && filter.value.goal !== task.goal) {
       return false;
     } else return true;
   })
@@ -63,13 +63,16 @@ const calendar = computed(() => {
   return calendar;
 });
 onMounted(() => {
-  const stored = localStorage.getItem(GOAL_FILTER_KEY);
-  if (stored) goal.value = parseInt(stored);
+  const data = localStorage.getItem(TASKS_FILTER_KEY);
+  if (data) filter.value = JSON.parse(data);
 });
-watch(goal, (data) => {
-  if (data) localStorage.setItem(GOAL_FILTER_KEY, data);
-  else localStorage.removeItem(GOAL_FILTER_KEY);
-});
+watch(
+  () => filter.value,
+  (data) => {
+    localStorage.setItem(TASKS_FILTER_KEY, JSON.stringify(data));
+  },
+  { deep: true }
+);
 </script>
 
 <template>
@@ -77,12 +80,34 @@ watch(goal, (data) => {
     <q-toolbar>
       <q-toolbar-title>Tasks</q-toolbar-title>
       <GoalSelect
-        v-model="goal"
+        v-model="filter.goal"
         label="For Goal"
         dense
         borderless
         class="col-6 col-sm-3 col-lg-2"
       />
+      <q-btn flat round icon="filter_list">
+        <q-menu>
+          <q-list>
+            <q-item>
+              <q-item-section>
+                <q-item-label>Hide passed done</q-item-label>
+              </q-item-section>
+              <q-item-section side>
+                <q-toggle v-model="filter.done" />
+              </q-item-section>
+            </q-item>
+            <q-item>
+              <q-item-section>
+                <q-item-label>Hide passed recurring</q-item-label>
+              </q-item-section>
+              <q-item-section side>
+                <q-toggle v-model="filter.recurring" />
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-menu>
+      </q-btn>
     </q-toolbar>
     <q-timeline v-if="calendar" layout="dense" class="q-px-md">
       <template v-for="(week, key) in calendar" :key="key">
