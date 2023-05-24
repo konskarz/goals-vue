5abe7bdd2a267099a2040d8c3c719a0e7243ec07
Konstantin Karzanov <k.karzanov@gmail.com>
2023-05-24 09:53:10 +0200

    add plus-one button

--- src/components/TaskListItem.vue
+++ src/components/TaskListItem.vue
@@ -1,9 +1,10 @@
 <script setup>
 import { computed } from 'vue'
-import { useQuasar } from 'quasar'
+// import { useQuasar } from 'quasar'
+import { useRouter } from 'vue-router'
 import { useTaskStore } from '../stores/TaskStore'
 import { fireworks } from '../lib/fireworks'
-import ProgressDialog from './ProgressDialog.vue'
+// import ProgressDialog from './ProgressDialog.vue'
 
 const props = defineProps({
   task: {
@@ -12,14 +13,15 @@ const props = defineProps({
   }
 })
 const emit = defineEmits(['ondragstart'])
-const $q = useQuasar()
+// const $q = useQuasar()
+const router = useRouter()
 const taskStore = useTaskStore()
 const showProgress = computed(() => props.task.target > 1 && !props.task.done)
 const progress = computed(() =>
   showProgress.value ? props.task.performance / props.task.target : 0
 )
 const caption = computed(() => [props.task.performance, 'of', props.task.target].join(' '))
-function showProgressDialog() {
+/* function showProgressDialog() {
   $q.dialog({
     component: ProgressDialog,
     componentProps: { task: props.task }
@@ -28,6 +30,18 @@ function showProgressDialog() {
     const changed = { performance: data.performance }
     taskStore.updateItem(props.task.id + '/', changed).then(() => taskStore.refetch())
   })
+} */
+function go() {
+  router.push('/tasks/' + props.task.id)
+}
+function undone() {
+  const changed = { performance: props.task.performance - 1, done: null }
+  taskStore.updateItem(props.task.id + '/', changed).then(() => taskStore.refetch())
+}
+function increase() {
+  const changed = { performance: props.task.performance + 1 }
+  if (changed.performance >= props.task.target) fireworks()
+  taskStore.updateItem(props.task.id + '/', changed).then(() => taskStore.refetch())
 }
 function onDragStart(event) {
   emit('ondragstart', event, props.task)
@@ -36,10 +50,7 @@ function onDragStart(event) {
 
 <template>
   <q-item clickable :active="Boolean(task.done)" active-class="text-positive">
-    <q-item-section v-if="task.done" thumbnail>
-      <q-icon name="done" color="positive" />
-    </q-item-section>
-    <q-item-section v-else-if="task.group_id" thumbnail>
+    <q-item-section v-if="task.group_id" thumbnail>
       <q-icon name="event_repeat" />
     </q-item-section>
     <q-item-section
@@ -52,12 +63,16 @@ function onDragStart(event) {
     >
       <q-icon name="drag_indicator" />
     </q-item-section>
-    <q-item-section @click="showProgressDialog">
+    <q-item-section @click="go">
       <q-item-label>{{ task.name }}</q-item-label>
       <q-item-label v-if="showProgress" caption>{{ caption }}</q-item-label>
       <q-item-label v-if="showProgress">
         <q-linear-progress :value="progress" color="positive" />
       </q-item-label>
+    </q-item-section>
+    <q-item-section avatar>
+      <q-btn v-if="task.done" flat round icon="done" @click="undone" />
+      <q-btn v-else flat round icon="plus_one" @click="increase" />
     </q-item-section>
   </q-item>
 </template>
