8fb3c3a4ff81e731a875ef5069ac8ba01f15184c
Konstantin Karzanov <k.karzanov@gmail.com>
2023-05-26 08:39:35 +0200

    code cleanup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { Quasar, Notify, Dialog } from 'quasar'
+import { Quasar, Notify } from 'quasar'
 
 import '@quasar/extras/material-icons/material-icons.css'
 import 'quasar/dist/quasar.css'
@@ -12,6 +12,6 @@ const app = createApp(App)
 
 app.use(router)
 app.use(createPinia())
-app.use(Quasar, { plugins: { Notify, Dialog } })
+app.use(Quasar, { plugins: { Notify } })
 
 app.mount('#app')
