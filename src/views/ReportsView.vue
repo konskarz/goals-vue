ac147f19ed2e3d41d95dca6a416e7ec71c6ca140
Konstantin Karzanov <k.karzanov@gmail.com>
2023-06-06 22:02:03 +0200

    fix heatmap

--- src/views/ReportsView.vue
+++ src/views/ReportsView.vue
@@ -1,26 +1,27 @@
 <script setup>
+import { computed } from 'vue'
 import { getCssVar } from 'quasar'
 import { useTaskStore } from '../stores/TaskStore'
 
 const store = useTaskStore()
-const options = {
+const heatmapOptions = {
   chart: {
     toolbar: { show: false }
   },
+  yaxis: {
+    labels: { maxWidth: 100 }
+  },
   dataLabels: { enabled: false },
-  colors: [getCssVar('positive')],
-  responsive: [
-    {
-      breakpoint: 600,
-      options: {
-        chart: { width: 568 },
-        yaxis: {
-          labels: { maxWidth: 100 }
-        }
-      }
-    }
-  ]
+  colors: [getCssVar('positive')]
 }
+const heatmapHeight = computed(() => 80 + store.recurring.length * 24)
+const heatmapWidth = computed(() => {
+  let length = 0
+  store.recurring.forEach((group) => {
+    if (group.data.length > length) length = group.data.length
+  })
+  return length > 0 ? 130 + length * 24 : '100%'
+})
 </script>
 
 <template>
@@ -28,13 +29,14 @@ const options = {
     <q-toolbar class="q-mt-md q-pl-lg">
       <q-toolbar-title>Reports</q-toolbar-title>
     </q-toolbar>
-    <div v-if="store.recurring" class="q-pl-lg q-pr-sm">
+    <div v-if="store.recurring" class="q-px-lg">
       <div class="text-subtitle2">Recurring Tasks</div>
       <apexchart
         type="heatmap"
-        :height="store.recurring.length * 44"
-        :options="options"
         :series="store.recurring"
+        :options="heatmapOptions"
+        :height="heatmapHeight"
+        :width="heatmapWidth"
         style="overflow: auto hidden"
       />
     </div>
