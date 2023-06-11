cfffe6dfdec8f3221be43fb3ff063dd5c988a924
Konstantin Karzanov <k.karzanov@gmail.com>
2023-06-11 16:06:25 +0200

    fix icon, code cleanup

--- src/views/MainLayout.vue
+++ src/views/MainLayout.vue
@@ -8,29 +8,13 @@ const router = useRouter()
 const route = useRoute()
 const pinia = getActivePinia()
 const store = useUserStore()
-const menuList = [
-  {
-    to: '/',
-    icon: 'pending_actions',
-    label: 'Plan'
-  },
-  {
-    to: '/goals',
-    icon: 'task_alt',
-    label: 'Goals'
-  },
-  {
-    to: '/tasks',
-    icon: 'done',
-    label: 'Tasks'
-  },
-  {
-    to: '/reports',
-    icon: 'auto_graph',
-    label: 'Reports'
-  }
+const views = [
+  { to: '/', icon: 'pending_actions', label: 'Plan' },
+  { to: 'tasks', icon: 'task_alt', label: 'Tasks' },
+  { to: 'goals', icon: 'outlined_flag', label: 'Goals' },
+  { to: 'reports', icon: 'auto_graph', label: 'Reports' }
 ]
-const drawerLeft = ref(false)
+const drawer = ref(false)
 function logout() {
   store.logout()
   router.push({ name: 'login', query: { next: route.fullPath } })
@@ -46,20 +30,16 @@ function logout() {
   <q-layout view="hhh LpR fff">
     <q-header reveal>
       <q-toolbar>
-        <q-btn flat round icon="menu" @click="drawerLeft = !drawerLeft" />
+        <q-btn flat round icon="menu" class="lt-md" @click="drawer = !drawer" />
         <q-toolbar-title>LifeTrackerBuddy</q-toolbar-title>
         <q-btn flat round icon="add">
           <q-menu auto-close>
             <q-list>
               <q-item :to="{ name: 'task', params: { id: 'new' } }">
-                <q-item-section>
-                  <q-item-label>New task</q-item-label>
-                </q-item-section>
+                <q-item-section><q-item-label>New task</q-item-label></q-item-section>
               </q-item>
               <q-item :to="{ name: 'goal', params: { id: 'new' } }">
-                <q-item-section>
-                  <q-item-label>New goal</q-item-label>
-                </q-item-section>
+                <q-item-section><q-item-label>New goal</q-item-label></q-item-section>
               </q-item>
             </q-list>
           </q-menu>
@@ -68,28 +48,20 @@ function logout() {
           <q-menu>
             <q-list>
               <q-item v-close-popup clickable @click="logout">
-                <q-item-section>
-                  <q-item-label>Logout</q-item-label>
-                </q-item-section>
+                <q-item-section><q-item-label>Logout</q-item-label></q-item-section>
               </q-item>
             </q-list>
           </q-menu>
         </q-btn>
       </q-toolbar>
     </q-header>
-    <q-drawer v-model="drawerLeft" show-if-above>
+    <q-drawer v-model="drawer" show-if-above bordered>
       <q-scroll-area class="fit">
         <q-list class="q-mt-md">
-          <template v-for="(menuItem, index) in menuList" :key="index">
-            <q-item :to="menuItem.to" exact>
-              <q-item-section avatar>
-                <q-icon :name="menuItem.icon" />
-              </q-item-section>
-              <q-item-section>
-                {{ menuItem.label }}
-              </q-item-section>
-            </q-item>
-          </template>
+          <q-item v-for="(view, index) in views" :key="index" :to="view.to" exact>
+            <q-item-section avatar><q-icon :name="view.icon" /></q-item-section>
+            <q-item-section>{{ view.label }}</q-item-section>
+          </q-item>
         </q-list>
       </q-scroll-area>
     </q-drawer>
