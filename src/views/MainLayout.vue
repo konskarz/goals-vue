4e7758322c6619ebd325f8c278529c50f2e50cbf
Konstantin Karzanov <k.karzanov@gmail.com>
2023-06-15 15:36:39 +0200

    fix drawer

--- src/views/MainLayout.vue
+++ src/views/MainLayout.vue
@@ -27,7 +27,7 @@ function logout() {
 </script>
 
 <template>
-  <q-layout view="hhh LpR fff">
+  <q-layout view="hHh LpR fFf">
     <q-header reveal :reveal-offset="0">
       <q-toolbar>
         <q-btn flat round icon="menu" class="lt-md" @click="drawer = !drawer" />
@@ -36,10 +36,10 @@ function logout() {
           <q-menu :offset="[0, 4]" auto-close>
             <q-list>
               <q-item :to="{ name: 'task', params: { id: 'new' } }">
-                <q-item-section><q-item-label>New task</q-item-label></q-item-section>
+                <q-item-section no-wrap><q-item-label>New task</q-item-label></q-item-section>
               </q-item>
               <q-item :to="{ name: 'goal', params: { id: 'new' } }">
-                <q-item-section><q-item-label>New goal</q-item-label></q-item-section>
+                <q-item-section no-wrap><q-item-label>New goal</q-item-label></q-item-section>
               </q-item>
             </q-list>
           </q-menu>
@@ -57,6 +57,9 @@ function logout() {
     </q-header>
     <q-drawer v-model="drawer" show-if-above>
       <q-scroll-area class="fit">
+        <q-toolbar class="lt-md bg-primary text-white">
+          <q-btn flat round dense icon="menu_open" @click="drawer = false" />
+        </q-toolbar>
         <q-list class="q-mt-md">
           <q-item v-for="(view, index) in views" :key="index" :to="view.to" exact>
             <q-item-section avatar><q-icon :name="view.icon" /></q-item-section>
