bd081677b063f7ec1cd8c0f892e9de5c8404d934
Konstantin Karzanov <k.karzanov@gmail.com>
2023-06-23 09:29:05 +0200

    add passed recurring average performance

--- src/components/GoalListItemSections.vue
+++ src/components/GoalListItemSections.vue
@@ -9,6 +9,7 @@ const caption = computed(() => [props.goal.performance, 'of', props.goal.target]
 const progress = computed(() =>
   showProgress.value ? props.goal.performance / props.goal.target : 0
 )
+const showAverage = computed(() => props.goal.average_performance !== null)
 </script>
 
 <template>
@@ -17,6 +18,13 @@ const progress = computed(() =>
     <q-item-label v-if="showProgress" caption>{{ caption }}</q-item-label>
     <q-item-label v-if="showProgress">
       <q-linear-progress :value="progress" color="positive" />
+    </q-item-label>
+    <q-item-label v-if="showAverage" caption>
+      <q-icon name="event_repeat" class="vertical-top" />
+      {{ Math.round(goal.average_performance * 100) }}%
+    </q-item-label>
+    <q-item-label v-if="showAverage">
+      <q-linear-progress :value="goal.average_performance" color="primary" />
     </q-item-label>
   </q-item-section>
 </template>
--- src/stores/GoalStore.js
+++ src/stores/GoalStore.js
@@ -1,4 +1,5 @@
 import { computed } from 'vue'
+import { date } from 'quasar'
 import { defineStore } from 'pinia'
 import { useCollection } from './collection'
 import { useTaskStore } from './TaskStore'
@@ -12,18 +13,38 @@ const arrayToTree = (array, parent = null) =>
 export const useGoalStore = defineStore('GoalStore', () => {
   const { data, refetch, getItem, createItem, updateItem, deleteItem } = useCollection('/goals/')
   const relatedStore = useTaskStore()
+  const currentDate = date.startOfDate(new Date(), 'day')
   const relative = computed(() => {
     if (!data.value || !relatedStore.data) return null
-    return data.value.map(({ ...item }) => {
+    return data.value.map((item) => {
       const branch = getBranch(item.id)
-      const tasks = relatedStore.data.filter((task) => !task.group_id && branch.includes(task.goal))
-      const done = tasks.filter((task) => task.done)
-      return { ...item, target: tasks.length, performance: done.length }
+      const tasks = relatedStore.data.filter((task) => branch.includes(task.goal))
+      const regular = tasks.filter((task) => !task.group_id)
+      const recurring = tasks.filter(
+        (task) =>
+          task.group_id && task.planned && new Date(task.planned.slice(0, 10)) <= currentDate
+      )
+      const done = regular.filter((task) => task.done)
+      return {
+        ...item,
+        target: regular.length,
+        performance: done.length,
+        average_performance: averagePerformance(recurring)
+      }
     })
   })
   const tree = computed(() => {
     return relative.value ? arrayToTree(relative.value) : null
   })
+  function averagePerformance(tasks) {
+    if (!tasks.length) return null
+    let perf = 0
+    tasks.forEach((task) => {
+      if (task.done) perf = perf + 1
+      else if (task.target > 1) perf = perf + task.performance / task.target
+    })
+    return perf / tasks.length
+  }
   function getBranch(itemId) {
     const branch = [itemId]
     const getChildren = (parent) =>
