5195db2b61b588cb57e1f920d454571e0a4759d6
Konstantin Karzanov <k.karzanov@gmail.com>
2023-07-14 04:37:30 +0200

    fix spacing

--- src/components/HomePane.vue
+++ src/components/HomePane.vue
@@ -7,16 +7,27 @@ const store = useTaskStore()
 </script>
 <template>
   <div>
-    <div class="q-px-lg row q-col-gutter-md">
+    <div class="row q-pl-lg q-pr-sm">
       <GoalSelect
         v-model="store.filter.goal"
         label="Filter by goal"
         :borderless="$q.screen.gt.xs"
         dense
-        class="col-12 col-sm"
+        hide-bottom-space
+        class="col-12 col-sm q-pb-md q-pr-md"
       />
-      <q-toggle v-model="store.filter.pastDone" label="Show past done" dense />
-      <q-toggle v-model="store.filter.pastRecurring" label="Show past recurring" dense />
+      <q-toggle
+        v-model="store.filter.pastDone"
+        label="Show past done"
+        dense
+        class="q-pb-md q-pr-md"
+      />
+      <q-toggle
+        v-model="store.filter.pastRecurring"
+        label="Show past recurring"
+        dense
+        class="q-pb-md q-pr-lg"
+      />
     </div>
     <ProgressItem v-if="store.progress" :item="store.progress" class="q-pb-lg q-px-lg" />
   </div>
