d0eba1f5b89dd96e88a1698fef35f6c367f4f2a5
Konstantin Karzanov <k.karzanov@gmail.com>
2024-07-18 21:33:06 +0200

    change baseURL, fix Notify messages

--- src/stores/ApiClient.js
+++ src/stores/ApiClient.js
@@ -6,23 +6,21 @@ import { Notify } from 'quasar'
 const baseURL = ((h) =>
   h === 'lifetrackerbuddy.com' || h === '127.0.0.1:8000' || h === 'localhost:8088'
     ? '/api/v2/'
-    : 'https://lifetrackerbuddy.com/api/v2/')(window.location.host)
+    : 'https://goals-fastify.vercel.app/')(window.location.host)
 const API = axios.create({ baseURL, headers: { 'content-type': 'application/json' } })
 
 export function useApiClient() {
   function handleSuccess(response) {
     Notify.create({
       color: 'positive',
-      message: response.statusText
+      message: response.status
     })
     return response.data
   }
   function handleError(error) {
     Notify.create({
       color: 'negative',
-      message: error.response
-        ? error.response.data.detail || error.response.data.non_field_errors
-        : error.message
+      message: error.response ? error.response.data.message : error.message
     })
     return Promise.reject(error)
   }
